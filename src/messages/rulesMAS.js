8c4dd1bea9056eeffbee67bacde8d065dbada834
Junior Zambrano <60367801+JuniorDZambra@users.noreply.github.com>
2023-10-14 17:45:44 -0400

    Update rulesMAS.js

--- src/messages/rulesMAS.js
+++ src/messages/rulesMAS.js
@@ -4,12 +4,11 @@ No nos hacemos responsables por suplantación de identidad, estafa, extorsión, 
 2. Al iniciar las celebraciones, la bola de cristal del vidente permitirá a cada participante ver a qué bando pertenece y a quién deberá darle una ofrenda. ¡Precaución! Esta visión es temporal y al apartar la mirada esta información no podrá ser vista de nuevo. Cómo es común en la aldea, los participantes no sabrán quiénes son lobos o aldeanos.
 3. Previo a la "Noche de la competencia".
 3.1 Cada cierto tiempo, el vidente seleccionará a un grupo de participantes:
-3.2 A cada participante seleccionado se le dará una lista con 3 opciones de integrantes del otro bando y deberá elegir uno de ellos para intentar adivinar a quién le toca ofrendar.
-3.3 Luego, deberá seleccionar el nombre de quién sospecha de entre una lista con los nombres de todos los participantes.
-3.4 El vidente contestará si el participante acertó la adivinanza o no. Pero cuidado, el vidente no te lo pondrá fácil, así que hay cierta probabilidad de que dé un falso positivo. De esta manera, los participantes sólo pueden estar seguros si la respuesta fue negativa.
-3.5 Esta dinámica durará hasta la selección de un siguiente grupo. Consumido ese tiempo, el vidente seleccionará a un nuevo conjunto de participantes.
+3.2 A cada participante seleccionado se le dará una lista con 3 opciones de integrantes del otro bando y deberá elegir uno de ellos para intentar adivinar si él es su MACamigo secreto.
+3.3 El vidente contestará si el participante acertó la adivinanza o no. Pero cuidado, el vidente no te lo pondrá fácil, así que hay cierta probabilidad de que dé un falso positivo. De esta manera, los participantes sólo pueden estar seguros si la respuesta fue negativa.
+3.4 Esta dinámica durará hasta la selección de un siguiente grupo. Consumido ese tiempo, el vidente seleccionará a un nuevo conjunto de participantes.
 4. "Noche de la competencia".
-4.1 El alcalde revelará los integrantes de cada bando.
+4.1 El alcalde organizará a los integrantes de cada bando.
 4.2 El alcalde seleccionará de forma aleatoria (sin reemplazo, hasta pasar por todos los integrantes) un integrante del bando que le toque adivinar.
 4.3 El participante seleccionará el nombre de quién cree que le ofrenda a él, de una lista de nombres de los integrantes del otro bando.
 4.4 El vidente dirá si acertó o no, y le dirá al alcalde que sume el puntaje.
