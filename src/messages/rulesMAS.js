30399f68e57e6ab9c6c232fc6f1b8077b3b9f251
Junior Zambrano <60367801+JuniorDZambra@users.noreply.github.com>
2023-10-30 08:48:56 -0400

    Update rulesMAS.js

--- src/messages/rulesMAS.js
+++ src/messages/rulesMAS.js
@@ -1,18 +1,32 @@
 const rules = `**REGLAS DEL JUEGO**
-1. Cuando llegan las fechas navideñas los *aldeanos* y los *lobos* vuelven a enfrentarse, pero esta vez para festejar una tradición milenaria conocida como **El MACamigo Secreto**. Durante este evento, ambos bandos dejan de lado sus instintos de supervivencia para regalarse ofrendas mutuamente y **TRABAJAN** para averiguar quién del otro bando le ofrenda al suyo. Cada *participante* deberá registrar su nombre en el *ayuntamiento* más cercano antes que inicien las celebraciones. Finalizada esta etapa no se admitirán adiciones ni modificaciones al listado final.
+
+> Cuando llegan las fechas navideñas los *aldeanos* y los *lobos* vuelven a enfrentarse, pero esta vez para festejar una tradición milenaria conocida como **El MACamigo Secreto**. Durante este evento, ambos *equipos* dejan de lado sus instintos de supervivencia para regalarse ofrendas mutuamente y **TRABAJAN** para averiguar quién del otro *equipo* le ofrenda al suyo. Cada *participante* deberá registrar su *nombre* en el *ayuntamiento* más cercano antes que inicien las celebraciones. Finalizada esta etapa no se admitirán adiciones ni modificaciones al listado final.
 No nos hacemos responsables por suplantación de identidad, estafa, extorsión, secuestro, heridas de mordidas, ser atravesado por un tridente o ser confundido por una hallaca por parte de un lobo.
-2. Al iniciar las celebraciones, el *alcalde* anunciará el bando al que pertenece cada *participante* y a quien deberá ofrendarle. También se podrá añadir una sugerencia de ofrenda.
-3. Previo a la **Noche de la competencia**.
-3.1 Cada cierto tiempo, la *vidente* seleccionará a un *grupo de participantes*:
-3.2 A cada *participante* seleccionado se le dará una lista con 3 opciones de integrantes del otro bando y deberá elegir uno de ellos para intentar adivinar si él es su **MACamigo Secreto**.
-3.3 La *vidente* contestará si el *participante* acertó la adivinanza o no. Pero cuidado, la *vidente* no te lo pondrá fácil, así que hay cierta probabilidad de que dé un falso positivo. De esta manera, los *participantes* sólo pueden estar seguros si la respuesta fue negativa.
-3.4 Esta dinámica durará hasta la selección de un siguiente grupo. Consumido ese tiempo, la *vidente* seleccionará a un nuevo conjunto de participantes.
-4. **Noche de la competencia**.
-4.1 Llegado el momento, el *alcalde* organizará a los integrantes de cada bando.
-4.2 El *alcalde* seleccionará de forma aleatoria (sin reemplazo, hasta pasar por todos los integrantes) un integrante del bando que le toque adivinar.
-4.3 El *participante* seleccionará el nombre de quién cree que le ofrenda a él, de una lista de nombres de los integrantes del otro bando.
-4.4 El *alcalde* dirá si acertó o no, y sumará el puntaje.
-4.5 Se continúa con el siguiente *participante*.
-4.6 Las celebraciones terminan cuando todos hayan adivinado o, en el peor de los casos, cuando todos los integrantes de un bando hayan sido devorados o linchados, y se anuncia el *bando ganador*, aquel que tuvo el *menor puntaje*.
+
+> Al iniciar las celebraciones, el *alcalde* anunciará el *equipo* al que pertenece cada *participante* y a quien deberá ofrendarle. También se podrá añadir una *sugerencia de ofrenda*.
+
+>> Previo a la **Noche de la competencia**.
+
+> Cada cierto tiempo, la *vidente* seleccionará a un *grupo de participantes*.
+
+> A cada *participante* seleccionado se le dará una lista con 3 opciones de integrantes del otro *equipo* y deberá elegir uno de ellos para intentar adivinar si él es su **MACamigo Secreto**.
+
+> La *vidente* contestará si el *participante* acertó la adivinanza o no. Pero cuidado, la *vidente* no te lo pondrá fácil, así que hay cierta probabilidad de que dé un falso positivo. De esta manera, los *participantes* sólo pueden estar seguros si la respuesta fue negativa.
+
+> Esta dinámica durará hasta la selección de un siguiente *grupo*. Consumido ese tiempo, la *vidente* seleccionará a un nuevo conjunto de participantes.
+
+>> **Noche de la competencia**.
+
+> Llegado el momento, el *alcalde* organizará a los integrantes de cada *equipo*.
+
+> El *alcalde* seleccionará de forma aleatoria (sin reemplazo, hasta pasar por todos los integrantes) un integrante del *equipo* que le toque adivinar.
+
+> El *participante* seleccionará el nombre de quién cree que le ofrenda a él, de una lista de nombres de los integrantes del otro *equipo*.
+
+> El *alcalde* dirá si acertó o no, y sumará el puntaje.
+
+> Se continúa con el siguiente *participante*.
+
+> Las celebraciones terminan cuando todos hayan adivinado o, en el peor de los casos, cuando todos los integrantes de un *equipo* hayan sido devorados o linchados, y se anuncia el *equipo ganador*, aquel que tuvo el *menor puntaje*.
 `
 export default rules
