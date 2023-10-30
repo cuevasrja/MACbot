3b631bbd98d91ff725fa68ef551ddd1e236e40e6
Junior Zambrano <60367801+JuniorDZambra@users.noreply.github.com>
2023-10-30 18:42:42 -0400

    Update MASMessages.js

--- src/messages/MASMessages.js
+++ src/messages/MASMessages.js
@@ -10,18 +10,18 @@ export const MAS_REGIST_ACTIVE = `El registro para el *MACamigo Secreto* está a
 
 export const MAS_REGIST_INACTIVE = `El registro para el *MACamigo Secreto* está inactivo. ¡Perdiste tu oportunidad de registrarte!`
 
-export const MAS_REGIST_DISABLED = `El registro para el **MACamigo Secreto** está desactivado.`
+export const MAS_REGIST_DISABLED = `El registro para el *MACamigo Secreto* está desactivado.`
 
-export const MAS_ALREADY_REGISTERED = `Ya estás registrado en el **MACamigo Secreto**.`
+export const MAS_ALREADY_REGISTERED = `Ya estás registrado en el *MACamigo Secreto*.`
 
-export const MAS_NOT_REGISTERED = `No estás registrado en el **MACamigo Secreto**.`
+export const MAS_NOT_REGISTERED = `No estás registrado en el *MACamigo Secreto*.`
 
-export const NOT_NAME_GIVEN = `No has introducido ningún *nombre*.`
+export const NOT_NAME_GIVEN = `No has introducido ningún _nombre_.`
 
-export const NAME_TOO_LONG = `El *nombre* es demasiado largo. Por favor, acórtalo.`
+export const NAME_TOO_LONG = `El _nombre_ es demasiado largo. Por favor, acórtalo.`
 
-export const NAME_USED = `Ya hay alguien con ese *nombre* en nuestros registros.`
+export const NAME_USED = `Ya hay alguien con ese _nombre_ en nuestros registros.`
 
-export const NOT_SUGGESTION_GIVEN = `No has introducido ninguna *sugerencia*.`
+export const NOT_SUGGESTION_GIVEN = `No has introducido ninguna _sugerencia_.`
 
 export const SUGGESTION_TOO_LONG = `La *sugerencia* es demasiado larga. Por favor, acórtala.`
