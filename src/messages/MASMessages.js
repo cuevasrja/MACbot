962c79c7b0aa2f169b44080c9905ec2c95b4f665
Junior Zambrano <60367801+JuniorDZambra@users.noreply.github.com>
2023-10-30 07:23:38 -0400

    Update MASMessages.js

--- src/messages/MASMessages.js
+++ src/messages/MASMessages.js
@@ -1,27 +1,27 @@
-export const MAS_PLAYING = `El MAC Amigo Secreto ya se está jugando.`
+export const MAS_PLAYING = `El **MACamigo Secreto** ya se está jugando.`
 
-export const MAS_NOT_PLAYING = `El MAC Amigo Secreto no se está jugando.`
+export const MAS_NOT_PLAYING = `El **MACamigo Secreto** no se está jugando.`
 
-export const MAS_FINISHED = `El MAC Amigo Secreto ha terminado. ¡Gracias a toda la aldea por participar!`
+export const MAS_FINISHED = `El **MACamigo Secreto** ha terminado. ¡Gracias a toda la aldea por participar!`
 
-export const MAS_RESET = `El MAC Amigo Secreto ha sido reiniciado.`
+export const MAS_RESET = `El **MACamigo Secreto ha sido reiniciado.`
 
-export const MAS_REGIST_ACTIVE = `El registro para el MAC Amigo Secreto está activo. ¡Regístrate con /MAS@add [nombre]!`
+export const MAS_REGIST_ACTIVE = `El registro para el **MACamigo Secreto** está activo. ¡Regístrate con /MAS@add [nombre]!`
 
-export const MAS_REGIST_INACTIVE = `El registro para el MAC Amigo Secreto está inactivo. ¡No te puedes registrar!`
+export const MAS_REGIST_INACTIVE = `El registro para el **MACamigo Secreto** está inactivo. ¡Perdiste tu oportunidad de registrarte!`
 
-export const MAS_REGIST_DISABLED = `El registro para el MAC Amigo Secreto está desactivado.`
+export const MAS_REGIST_DISABLED = `El registro para el **MACamigo Secreto** está desactivado.`
 
-export const MAS_ALREADY_REGISTERED = `Ya estás registrado en el MAC Amigo Secreto.`
+export const MAS_ALREADY_REGISTERED = `Ya estás registrado en el **MACamigo Secreto**.`
 
-export const MAS_NOT_REGISTERED = `No estás registrado en el MAC Amigo Secreto.`
+export const MAS_NOT_REGISTERED = `No estás registrado en el **MACamigo Secreto**.`
 
-export const NOT_NAME_GIVEN = `No has introducido ningún nombre.`
+export const NOT_NAME_GIVEN = `No has introducido ningún *nombre*.`
 
-export const NAME_TOO_LONG = `El nombre es demasiado largo. Por favor, acórtalo.`
+export const NAME_TOO_LONG = `El *nombre* es demasiado largo. Por favor, acórtalo.`
 
-export const NAME_USED = `Ya hay alguien con ese nombre en nuestros registros.`
+export const NAME_USED = `Ya hay alguien con ese *nombre* en nuestros registros.`
 
-export const NOT_SUGGESTION_GIVEN = `No has introducido ninguna sugerencia.`
+export const NOT_SUGGESTION_GIVEN = `No has introducido ninguna *sugerencia*.`
 
-export const SUGGESTION_TOO_LONG = `La sugerencia es demasiado larga. Por favor, acórtala.`
+export const SUGGESTION_TOO_LONG = `La *sugerencia* es demasiado larga. Por favor, acórtala.`
